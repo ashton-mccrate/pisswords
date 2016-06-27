d529d8bfc311aaa82fae105acb7ce909efa12e1d
Ashton McCrate <a.mccrate@gmail.com>
2016-06-27 15:37:26 -0700

    add methods for encoding and decoding

--- lib/pissword.rb
+++ lib/pissword.rb
@@ -3,8 +3,8 @@
 
 module Pissword
 
+	### The name of the file which holds the dictionary
 	DEFAULT_DICTIONARY_FILE_NAME = 'dictionary.txt'
-
 
 	###############
 	module_function
@@ -14,6 +14,13 @@ module Pissword
 	### Return the file that has the dictionary of words
 	def dictionary_file
 		return Pathname( __FILE__ ).dirname + DEFAULT_DICTIONARY_FILE_NAME
+	end
+
+
+	### Return the list of words to choose from. If there isn't one, read the
+	### words from the dictionary file
+	def word_list
+		return @word_list ||= read_words
 	end
 
 
@@ -30,6 +37,20 @@ module Pissword
 		end
 
 		return words
+	end
+
+
+	### Encode the +secrets+ you don't want people to know about.
+	### Returns the secrets hashed in to dirty words.
+	def encode( secrets )
+		return secrets.bytes.map{|byte| word_list[byte] }.join( ' ' )
+	end
+
+
+	### Decode the given +pisswords+ to return the original secret
+	def decode( pisswords )
+		bytes = pisswords.split( ' ' ).map{|word| word_list.index( word )}
+		return bytes.pack( 'C*' )
 	end
 
 end
--- spec/pissword_spec.rb
+++ spec/pissword_spec.rb
@@ -9,4 +9,23 @@ describe Pissword do
 		expect( words ).to all( be_a( String ) )
 	end
 
+
+	describe "encoding and decoding" do
+
+		let( :secret ) { "I hate the way your butt looks in those shorts." }
+
+
+		it "can encode your secrets" do
+			encoded = Pissword.encode( secret )
+			expect( encoded ).to be_a( String )
+			expect( encoded ).to_not eq( secret )
+		end
+
+
+		it "can decode your pisswords" do
+			encoded = Pissword.encode( secret )
+			expect( Pissword.decode( encoded) ).to eq( secret )
+		end
+	end
+
 end
